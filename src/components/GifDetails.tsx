b27794e503f0e70024b75118cd0dbc98500fc476
--set <--set>
2022-03-07 00:13:13 -0500

    Use downsized gif images to increases performance

--- src/components/GifDetails.tsx
+++ src/components/GifDetails.tsx
@@ -5,7 +5,7 @@ import {useDecodedGif} from '../hooks/useDecodedGif';
 import {useGifFrames} from "../hooks/useGifFrames";
 
 export function GifDetails(props: {item: IGif, index: number}) {
-  const {frames = [], isDecoded} = useDecodedGif(props.item.images.original.url, props.item.slug);
+  const {frames = [], isDecoded} = useDecodedGif(props.item.images.downsized.url, props.item.slug);
   const {currentFrame} = useGifFrames(0, frames, 60);
 
   const md = `<img src="${currentFrame}">`;
--- src/components/GifResult.tsx
+++ src/components/GifResult.tsx
@@ -4,13 +4,15 @@ import {Action, ActionPanel, List} from "@raycast/api";
 import {GifDetails} from './GifDetails';
 
 export function GifResult(props: {item: IGif, index: number}) {
+  const {id, title, images} = props.item;
+
   return (
     <List.Item
-      key={props.item.id}
-      title={props.item.title}
-      icon={{source: props.item.images.original.url}}
+      key={id}
+      title={title}
+      icon={{source: images.preview_gif.url}}
       actions={
-        <ActionPanel title={props.item.title}>
+        <ActionPanel title={title}>
           <Action.Push
             title="Show Details"
             target={<GifDetails item={props.item} index={props.index} />}
